370e19bd2cef3d4be58bf7d221fcd02622a18ef2
Mackie <masaki.shimadu@gmail.com>
2023-06-08 14:33:27 +0900

    feat: first commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TaskModule,
+    PrismaModule,
+    UserModule,
+  ],
+})
+export class AppModule {}
