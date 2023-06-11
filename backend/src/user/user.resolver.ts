58a817ff77821eb8dcfb773d6b276c31891a25f9
Mackie <masaki.shimadu@gmail.com>
2023-06-11 21:59:31 +0900

    feat: user

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/createUser.input';
+import { User } from '@prisma/client';
+import { User as UserModel } from './models/user.model';
+import { GetUserArgs } from './dto/getUser.args';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-aut.guard';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserModel)
+  @UseGuards(JwtAuthGuard)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return await this.userService.createUser(createUserInput);
+  }
+
+  @Query(() => UserModel, { nullable: true })
+  @UseGuards(JwtAuthGuard)
+  async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
+    return await this.userService.getUser(getUserArgs.email);
+  }
+}
