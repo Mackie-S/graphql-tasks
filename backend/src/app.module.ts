d31167b8bd641baa1bb22ecf83438b365d814a70
Mackie <masaki.shimadu@gmail.com>
2023-06-11 22:00:21 +0900

    feat: auth at app mod

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -15,6 +16,7 @@ import { UserModule } from './user/user.module';
     TaskModule,
     PrismaModule,
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
